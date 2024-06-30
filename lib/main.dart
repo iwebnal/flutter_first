f752aa29febedfbe6c551deb165e29ad4f902d5f
iwebnal <iwebnal@gmail.com>
2024-06-30 20:24:31 +0300

    Start use MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-      "Hello Flutter!",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-          fontWeight: FontWeight.bold, color: Colors.red, fontSize: 40),
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello Flutter!",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.red, fontSize: 40),
+        ),
+      ),
     ),
-  ));
+  );
 }
